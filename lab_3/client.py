1761541097ebe58431b2596ccdd64218d7101072
Erik Perillo <erik.perillo@gmail.com>
2016-03-29 21:43:52 -0300

    finished

--- lab_3/client.py
+++ lab_3/client.py
@@ -3,49 +3,69 @@
 import socket
 import thread
 import oarg
-import time
 import sys
 
 class Client(object):
-    def __init__(self, server_host, server_port, recv_buffer=4096):
+    """
+    this class represent client side in the chat.
+    it connects to the server and can send and receive messages.    
+    """
+    def __init__(self, server_host, server_port):
+        """
+        variables initialization and socket setup.
+        """
         self.server_host = server_host
         self.server_port = server_port
-        self.recv_buffer = recv_buffer
         #starting socket
         self.start_socket()
         #connecting to server
         self.connect_to_server()
 
     def start_socket(self):
+        """
+        starts socket with apropriate configurations.
+        """
         #creating socket    
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
         #use already occupied address
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def connect_to_server(self):
+        """
+        starts connection with chat server.
+        """
         self.server_socket.connect((self.server_host, self.server_port))
 
     def main_loop(self):
+        """
+        spams a thread to listen to messages from other clients and
+        then keeps waiting for user input.
+        """
         thread.start_new(listener, (self.server_socket,))
         while True:
-            data = raw_input("insert message: ")
+            data = raw_input()
             self.server_socket.sendall(data)
 
     def __del__(self):
-        self.socket.close()
+        self.server_socket.close()
 
 def listener(socket, recv_buffer=4096):
+    """
+    worker thread calls this function to listen to other clients.
+    """
     while True:
-        #sys.stdout.write("insert message: ")
-        #sys.stdout.flush()
+        sys.stdout.write(">>> ")
+        sys.stdout.flush()
         data = socket.recv(recv_buffer)
+        if not data:
+            break
         print data
 
 def main():
-    port = oarg.Oarg("-p --port", 1234, "server port")
+    #command line arguments
+    port = oarg.Oarg("-p --port", 5050, "server port")
     host = oarg.Oarg("-h --host", "", "server host")
-    message = oarg.Oarg("-m --message", "ey b0ss", "message to send")
-    hlp = oarg.Oarg("-h --help", False, "this help message")
+    hlp = oarg.Oarg("--help", False, "this help message")
 
     oarg.parse()
 
@@ -53,12 +73,17 @@ def main():
         oarg.describeArgs("options:")
         exit()
 
-    print "starting server ..."
+    print " --- setting up client ---" 
     client = Client(host.val, port.val)
-    print "conneted to", (client.server_host, client.server_port)
+    print " --- conneted to server '%s', port %d ---" % \
+        (client.server_host, client.server_port)
 
-    print "starting server main loop"
-    client.main_loop()
+    print " --- starting client main loop. type as you wish! ---"
+    try:
+        client.main_loop()
+    except Exception as e:
+        print " --- connection error: '%s' --- " % e.message
+        exit()
 
 if __name__ == "__main__":
     main()
--- lab_3/server.py
+++ lab_3/server.py
@@ -5,7 +5,15 @@ import thread
 import oarg
 
 class Server(object):
+    """
+    class representing chat server.
+    the server's main function is to receive incomming connections and
+    broadcast them to all other clients.
+    """
     def __init__(self, host, port, recv_buffer=4096):
+        """
+        variables initialization and socket setup.
+        """
         self.connections = []
         self.host = host
         self.port = port
@@ -16,6 +24,9 @@ class Server(object):
         self.listen()
 
     def start_socket(self):
+        """
+        starts socket with apropriate configurations.
+        """
         #creating socket    
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
         #use already occupied address
@@ -24,32 +35,62 @@ class Server(object):
         self.socket.bind((self.host, self.port))
 
     def listen(self, backlog=16):
+        """
+        sets up socket to become a server.
+        """
         #becoming a server
         self.socket.listen(backlog)
 
     def main_loop(self):
-         while True:
+        """
+        main server loop where server receives connections and spams them
+        to worker threads were it handles messages.
+        """
+        while True:
             client_socket, client_address = self.socket.accept()
-            print "address:", client_address
+            print " --- new connection to %s, port %d ---" % \
+                (client_address[0], client_address[1])
             self.connections.append((client_socket, client_address))
+            #worker thread
             thread.start_new(chat, (len(self.connections)-1, self,
                 client_socket)) 
 
     def broadcast(self, data):
+        """
+        sends message to all connected clients.
+        """
         print data
-        for (client_socket, __) in self.connections:
-            client_socket.sendall(data) 
+        for i, (client_socket, address) in enumerate(self.connections):
+            try:
+                client_socket.sendall(data) 
+            except Exception as e:
+                print " --- connection error in socket #%d: %s ---" % \
+                    (i, e.message)
+                client_socket.close()
+                self.connections.remove((client_socket, address))
 
     def __del__(self):
+        for (client_socket, __) in self.connections:
+            client_socket.close()
         self.socket.close()
 
 def chat(thr_id, server, client_socket, recv_buffer=4096):
+    """
+    connetion handler: each connection spams a thread that calls this 
+    function.
+    """
     while True:
-        data = client_socket.recv(recv_buffer)
-        server.broadcast("message from #%d: %s" % (thr_id, data))
+        try:
+            data = client_socket.recv(recv_buffer)
+            if data:
+                server.broadcast(" ---> message from #%d: %s <---" \
+                    % (thr_id, data))
+        except:
+            break
 
 def main():
-    port = oarg.Oarg("-p --port", 1234, "port to start server")
+    #command line arguments
+    port = oarg.Oarg("-p --port", 5050, "port to start server")
     host = oarg.Oarg("-h --host", "", "host to start server")
     recv_buffer = oarg.Oarg("-b --buffer", 4096, "receiver buffer size")
     hlp = oarg.Oarg("--help", False, "this help message")
@@ -60,12 +101,16 @@ def main():
         oarg.describeArgs("options:")
         exit()
 
-    print "starting server ..."
+    print " --- starting server ---"
     server = Server(host.val, port.val, recv_buffer.val)
-    print "started server on port #%d" % server.port
+    print " --- started server on port %d ---" % server.port
 
-    print "starting server main loop"
-    server.main_loop()
+    print " --- starting server main loop ---"
+    try:
+        server.main_loop()
+    except KeyboardInterrupt:
+        print " --- service terminated ---"  
+        exit()
 
 if __name__ == "__main__":
     main()
